0d40a29f85e5f042c007bb3aa8e591d0f768f769
Gabriela Ramirez Valencia <gabrielaaa1413@gmail.com>
2019-04-10 13:07:46 -0500

    estructura general del proyecto

---
+++ src/components/Panel/Panel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+
+import './Panel.sass';
+const Panel = () => (
+    <div>
+        <h1> Panel de control </h1>
+        <Menu inverted>
+            <Menu.Item   >
+            <Link to="/posts">
+                Posts
+                </Link>
+            </Menu.Item>
+
+            <Menu.Item   >
+            <Link to="/albums">
+                Albums
+                </Link>
+            </Menu.Item>
+
+          </Menu>
+    </div>
+    
+);
+
+export default Panel;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+
+    apiKey: "AIzaSyD5TmpzLsEJcghd3fv4LWndf8LHhNFfD_A",
+    authDomain: "react-eam-bf189.firebaseapp.com",
+    databaseURL: "https://react-eam-bf189.firebaseio.com",
+    projectId: "react-eam-bf189",
+    storageBucket: "react-eam-bf189.appspot.com",
+    messagingSenderId: "428381034239"
+}
